2ca560fee8f021060130c17c6843ec456a408127
Hamza Sultan <hamzasultan9662@gmail.com>
2025-05-16 17:14:23 +0500

    clear

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"  # ← apna real bucket name likho
+    bucket = "my-terraform-state-bucket-007-hamza"  # ← apna real bucket name likho
     key    = "dev/terraform.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
   }
 }
